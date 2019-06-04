23686f1b8e7ab96c44fa616cad630b273d9670ce
Mayeul Sevez d'Alzon <MASED8@ulaval.ca>
2019-06-04 09:52:56 -0400

    add runnable per;ission to assemble

---
+++ r-minimal-minimal-notebook/jupyter_notebook_config.py
@@ -0,0 +1,33 @@
+import os
+
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8080
+c.NotebookApp.open_browser = False
+c.NotebookApp.quit_button = False
+
+password = os.environ.get('JUPYTER_NOTEBOOK_PASSWORD')
+if password:
+    import notebook.auth
+    c.NotebookApp.password = notebook.auth.passwd(password)
+    del password
+    del os.environ['JUPYTER_NOTEBOOK_PASSWORD']
+
+image_config_file = '/opt/app-root/src/.jupyter/jupyter_notebook_config.py'
+
+if os.path.exists(image_config_file):
+    with open(image_config_file) as fp:
+        exec(compile(fp.read(), image_config_file, 'exec'), globals())
+
+
+# S3 configuration
+from s3contents import S3ContentsManager
+
+c = get_config()
+
+# Tell Jupyter to use S3ContentsManager for all storage.
+c.NotebookApp.contents_manager_class = S3ContentsManager
+c.S3ContentsManager.access_key_id = os.environ.get("S3_ACCESS_KEY_ID")
+c.S3ContentsManager.secret_access_key = os.environ.get("S3_SECRET_ACCESS_KEY")
+c.S3ContentsManager.endpoint_url = os.environ.get("S3_ENPOINT_URL")
+c.S3ContentsManager.bucket = os.environ.get("S3_BUCKET")
+c.S3ContentsManager.prefix = os.environ.get("JUPYTERHUB_USER")
